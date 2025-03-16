274c76757b012f21ff7d836330a7a7f2261ad0e1
ORGIsEvil <vasya.cherny.2006@mail.ru>
2025-03-16 16:37:49 +0500

    add-front-login-page

---
+++ CockUI/src/scripts/sign.js
@@ -0,0 +1,39 @@
+const loginUrl = "/login";
+console.log("Check");
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (!username || !password) {
+        errorMessage.textContent = 'Пожалуйста, заполните все поля.';
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email, password: password }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                console.log('succsess: ', data.success);
+                window.location.href = "/mainpage";
+            } else {
+                errorMessage.textContent = data.message || 'Ошибка авторизации';
+                errorMessage.style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+            errorMessage.textContent = 'Произошла ошибка при отправке данных.';
+            errorMessage.style.display = 'block';
+        });
+});
