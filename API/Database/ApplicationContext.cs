f81f7d19bc797315fc8a9c880ca5aaa6dd410fa0
ORGIsEvil <vasya.cherny.2006@mail.ru>
2025-03-18 15:34:45 +0500

    change-first-page

--- API/Database/ApplicationContext.cs
+++ API/Database/ApplicationContext.cs
@@ -11,10 +11,7 @@ public class ApplicationContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasKey(x => x.Id);
-        modelBuilder.Entity<User>().HasData(
-            new User { Id=256, Email="test1@jmail.com", Password = "1234", IsAdmin = false }, 
-            new User { Id=257, Email="admin@jmail.com", Password = "QwErTy", IsAdmin = true },
-            new User { Id=258, Email="test2@gandex.ru", Password = "15", IsAdmin = false }
-        );
+        modelBuilder.Entity<User>().Property(x => x.Id)
+            .ValueGeneratedOnAdd();
     }
 }
