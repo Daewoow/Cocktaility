258a97a9b1154a1fab071aa1d4657333e9372909
ORGIsEvil <vasya.cherny.2006@mail.ru>
2025-03-31 19:17:03 +0500

    add FavoriteBarsController

---
+++ API/Controllers/FavoriteBarsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+
+namespace API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class FavoriteBarsController(BarService barService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Bar>>> GetAllBars()
+    {
+        var userId = User.Identity.Name;
+        
+        try
+        {
+            var bars = await barService.GetFavoriteBars(userId);
+            return Ok(bars);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Произошла ошибка при обработке запроса");
+        }
+    }
+
+    [HttpPost("{barId}")]
+    public async Task<IActionResult> AddToFavorites(int barId)
+    {
+        var userId = User.Identity.Name;
+        
+        try
+        {
+            var success = await barService.AddToFavorites(userId, barId);
+            if (success)
+                return Ok();
+            return StatusCode(500, "Не найден пользователь или бар");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Произошла ошибка при обработке запроса");
+        }
+    }
+
+    [HttpDelete("{barId}")]
+    public async Task<IActionResult> RemoveFromFavorites(int barId)
+    {
+        var userId = User.Identity.Name;
+        
+        try
+        {
+            var success  = await barService.RemoveFromFavorites(userId, barId);
+            if (success)
+                return Ok();
+            return StatusCode(500, "Не найден пользователь или бар");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Произошла ошибка при обработке запроса");
+        }
+    }
+}
--- API/Services/BarService.cs
+++ API/Services/BarService.cs
@@ -52,4 +52,83 @@ public class BarService
     {
         return await _context.Tags.Select(t => t.Name).ToListAsync();
     }
+
+    public async Task<List<Bar>> GetFavoriteBars(string userId)
+    {
+        var favoriteBars = await _context.Favorites
+            .Where(f => f.UserId == userId)
+            .Join(
+                _context.Bars,
+                f => f.BarId,
+                b => b.BarId,
+                (favorite, bar) => bar)
+            .OrderBy(b => b.BarId)
+            .ToListAsync();
+
+        return favoriteBars;
+    }
+
+    public async Task<bool> AddToFavorites(string userId, int barId)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+        if (user == null)
+        {
+            return false;
+        }
+        
+        var bar = await _context.Bars.FirstOrDefaultAsync(x => x.BarId == barId);
+        if (bar == null)
+        {
+            return false;
+        }
+
+        var alreadyFavorite = await _context.Favorites
+            .AnyAsync(f => f.BarId == barId && f.UserId == userId);
+        
+        if (alreadyFavorite)
+        {
+            return true;
+        }
+        
+        var favoriteBar = new Favorite
+        {
+            UserId = userId,
+            User = user,
+            BarId = barId,
+            Bar = bar
+        };
+
+        _context.Favorites.Add(favoriteBar);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+    
+    public async Task<bool> RemoveFromFavorites(string userId, int barId)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+        if (user == null)
+        {
+            return false;
+        }
+        
+        var bar = await _context.Bars.FirstOrDefaultAsync(x => x.BarId == barId);
+        if (bar == null)
+        {
+            return false;
+        }
+        
+        var favoriteBar = await _context.Favorites
+            .FirstOrDefaultAsync(f => f.BarId == barId && f.UserId == userId);
+        
+        if (favoriteBar == null)
+        {
+            return true;
+        }
+
+        _context.Favorites.Remove(favoriteBar);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
 }
